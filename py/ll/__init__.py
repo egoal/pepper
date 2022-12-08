94302687e6ea3823f082fecaaf01d6b654c90b5c
egoal <939425550@qq.com>
2022-12-08 18:00:18 +0800

    add gu in ll.

--- py/ll/__init__.py
+++ py/ll/__init__.py
@@ -1,3 +1,4 @@
 from .ll import *
 from . import calc
 from . import utils2d
+from . import geodesic_utils as gu
--- py/ll/geodesic_utils.py
+++ py/ll/geodesic_utils.py
@@ -1,107 +1,58 @@
+import sys
+import os
 import math
 import numpy as np
 
-RADIUS = 6378137.0      # semi-major axis
-e = 1 / 298.257223563   # flattening
-e1s = 0.00669437999013  # e1 sqaure
-wie = 2.0 * math.pi / (23.0 * 3600.0 + 56.0 * 60.0 + 4.0)
 
-
-def ll2Rne(lon, lat) -> np.ndarray:
-    '''
-    @return 3x3 rotation matrix
-    '''
-    clon, slon = math.cos(lon), math.sin(lon)
-    clat, slat = math.cos(lat), math.sin(lat)
-
-    return np.c_[[-slon, clon, 0], [-slat*clon, -slat*slon, clat], [clat*clon, clat*slon, slat]]
-
-
-def lla2xyz(lla) -> np.ndarray:
-    '''
-    @param lla: Nx3 [lon, lat, alt] 
-    @return Nx3 [x, y, z] in ecef
-    '''
-    lon, lat, alt = lla[:, 0], lla[:, 1], lla[:, 2]
-
-    clon, slon = np.cos(lon), np.sin(lon)
-    clat, slat = np.cos(lat), np.sin(lat)
-
-    N = RADIUS/np.sqrt(clat**2 + (1-e)**2 * slat**2)
-    h = N+alt
-    return np.hstack((h * clat * clon, h * clat*slon, (N*(1-e)**2 + alt)*slat))
-
-
-def xyz2enu(xyz, anchor) -> np.ndarray:
-    '''
-    @param xyz: Nx3
-    @anchor 3 [lon, lat, alt]
-    '''
-    return (xyz - lla2xyz(anchor.reshape((-1, 3)))) @ (ll2Rne(anchor[0], anchor[1])).T
-
-
-def lla2enu(lla, anchor):
-    '''
-    compose: xyz2enu \circ lla2xyz
-    '''
-    return xyz2enu(lla2xyz(lla), anchor)
-
-
-def xyz2lla(xyz) -> np.ndarray:
-    '''
-    @param xyz: Nx3
-    '''
-    x, y, z = xyz[:, 0], xyz[:, 1], xyz[:, 2]
-    L = np.arctan2(y, x)
-    tmpB = z / np.sqrt(x**2 + y**2)
-    B = np.arctan(tmpB/(1-e)**2)
-
-    iter, max_iterations = 0, 500
-    err, thresh = 1, 1e-15
-    while iter < max_iterations and err > thresh:
-        R1 = x / (np.cos(L) * np.cos(B))
-        R2 = RADIUS / np.sqrt(1 - e1s * np.sin(B)**2)
-        newB = np.arctan(tmpB * R1 / (R1-R2*e1s))
-        err = max(abs(newB - B))
-        B = newB
-
-    return np.vstack((L, B, R1-R2)).T
-
-
-def enu2xyz(enu, anchor) -> np.ndarray:
-    '''
-    @param enu: Nx3
-    '''
-    return enu @ ll2Rne(anchor[0], anchor[1]).T + lla2xyz(np.r_[anchor[0], anchor[1], 0.].reshape((-1, 3)))
-
-
-def enu2lla(enu, anchor):
-    return xyz2lla(enu2xyz(enu, anchor))
-
-# wgs, gcj
+def in_china(lng, lat):
+    return lng > 73.66 and lng < 135.05 and lat > 3.86 and lat < 53.55
 
 
 def _transform_latitude(lng, lat):
-    ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * \
-        lat + 0.1 * lng * lat + 0.2 * np.sqrt(np.fabs(lng))
-    ret += (20.0 * np.sin(6.0 * lng * np.pi) + 20.0 *
-            np.sin(2.0 * lng * np.pi)) * 2.0 / 3.0
+    ret = (
+        -100.0
+        + 2.0 * lng
+        + 3.0 * lat
+        + 0.2 * lat * lat
+        + 0.1 * lng * lat
+        + 0.2 * np.sqrt(np.fabs(lng))
+    )
+    ret += (
+        (20.0 * np.sin(6.0 * lng * np.pi) + 20.0 * np.sin(2.0 * lng * np.pi))
+        * 2.0
+        / 3.0
+    )
     ret += (20.0 * np.sin(lat * np.pi) + 40.0 *
             np.sin(lat / 3.0 * np.pi)) * 2.0 / 3.0
-    ret += (160.0 * np.sin(lat / 12.0 * np.pi) +
-            320 * np.sin(lat * np.pi / 30.0)) * 2.0 / 3.0
+    ret += (
+        (160.0 * np.sin(lat / 12.0 * np.pi) + 320 * np.sin(lat * np.pi / 30.0))
+        * 2.0
+        / 3.0
+    )
     return ret
 
 
 def _transform_longitude(lng, lat):
-    ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + \
-        0.1 * lng * lat + 0.1 * np.sqrt(np.fabs(lng))
-    ret += (20.0 * np.sin(6.0 * lng * np.pi) + 20.0 *
-            np.sin(2.0 * lng * np.pi)) * 2.0 / 3.0
+    ret = (
+        300.0
+        + lng
+        + 2.0 * lat
+        + 0.1 * lng * lng
+        + 0.1 * lng * lat
+        + 0.1 * np.sqrt(np.fabs(lng))
+    )
+    ret += (
+        (20.0 * np.sin(6.0 * lng * np.pi) + 20.0 * np.sin(2.0 * lng * np.pi))
+        * 2.0
+        / 3.0
+    )
     ret += (20.0 * np.sin(lng * np.pi) + 40.0 *
             np.sin(lng / 3.0 * np.pi)) * 2.0 / 3.0
-    ret += (150.0 * np.sin(lng / 12.0 * np.pi) +
-            300.0 * np.sin(lng / 30.0 * np.pi)) * 2.0 / 3.0
+    ret += (
+        (150.0 * np.sin(lng / 12.0 * np.pi) + 300.0 * np.sin(lng / 30.0 * np.pi))
+        * 2.0
+        / 3.0
+    )
     return ret
 
 
@@ -110,7 +61,7 @@ def _transform(lng, lat):
     ee = 0.00669342162296594323
     dlat = _transform_latitude(lng - 105.0, lat - 35.0)
     dlng = _transform_longitude(lng - 105.0, lat - 35.0)
-    radlat = lat / 180.0 * np.pi
+    radlat = np.radians(lat)
     magic = np.sin(radlat)
     magic = 1 - ee * magic * magic
     sqrtmagic = np.sqrt(magic)
@@ -122,20 +73,161 @@ def _transform(lng, lat):
     return lng, lat
 
 
-def wgs2gcj(wgs):
+def wgs2gcj(lng, lat):
     '''
-    @param wgs: Nx2 radians
+    input & return `lng`, `lat` in degrees
+    note that `lng` and `lat` can be vector
     '''
-    gcj = np.degrees(wgs)
-    gcj[:, 0], gcj[:, 1] = _transform(gcj[:, 0], gcj[:, 1])
-    return np.radians(gcj)
+    assert in_china(lng, lat), 'not in china'
+    return _transform(lng, lat)
 
 
-def gcj2wgs(gcj):
+def gcj2wgs(lng, lat):
     '''
-    @param wgs: Nx2 radians
+    input & return `lng`, `lat` in degrees
+    note that `lng` and `lat` can be vector
     '''
-    wgs = np.degrees(gcj)
-    wgs[:, 0], wgs[:, 1] = _transform(wgs[:, 0], wgs[:, 1])
-    wgs = np.radians(wgs)
-    return gcj*2 - wgs
+    newlng, newlat = _transform(lng, lat)
+    return lng*2 - newlng, lat*2-newlat
+
+
+def wgs_distance(lng1: float, lat1: float, lng2: float, lat2: float):
+    '''
+    input degrees, return distance on sphere
+    this should not be very accurate.
+    '''
+    earth_radius_km = 6371.0
+    lng1r = np.radians(lng1)
+    lat1r = np.radians(lat1)
+    lng2r = np.radians(lng2)
+    lat2r = np.radians(lat2)
+    u = np.sin((lat2r - lat1r) / 2)
+    v = np.sin((lng2r - lng1r) / 2)
+    return (
+        1000.0
+        * 2.0
+        * earth_radius_km
+        * np.arcsin(np.sqrt(u ** 2 + np.cos(lat1r) * np.cos(lat2r) * v * v))
+    )
+
+
+def ll2Rne(lon: float, lat: float):
+    '''
+    get rotation at (`lon`, `lat`), in degrees
+    '''
+    lon, lat = math.radians(lon), math.radians(lat)
+    c_lat = np.cos(lat)
+    s_lat = np.sin(lat)
+    c_lon = np.cos(lon)
+    s_lon = np.sin(lon)
+    return np.array(
+        [
+            [-s_lon, -s_lat * c_lon, c_lat * c_lon],
+            [c_lon, -s_lat * s_lon, c_lat * s_lon],
+            [0, c_lat, s_lat],
+        ],
+        dtype=np.float64,
+    )
+
+
+def lla2xyz(lon, lat, alt):
+    '''
+    `lon`, `lat` in degrees, alt in meters. return x, y, z
+    note that input can be vector
+    '''
+    lonr, latr = np.radians(lon), np.radians(lat)
+    RADIUS = 6378137  # semi-major axis
+    e = 1 / 298.257223563  # flattening
+    N = RADIUS / np.sqrt(np.cos(latr) * np.cos(latr) + (1 - e)
+                         ** 2 * np.sin(latr) * np.sin(latr))
+    h = N + alt
+
+    x = h * np.cos(latr) * np.cos(lonr)
+    y = h * np.cos(latr) * np.sin(lonr)
+    z = (N * (1 - e) ** 2 + alt) * np.sin(latr)
+    return x, y, z
+
+
+def xyz2lla(x, y, z):
+    '''
+    `x`, `y`, `z` can be vector, 
+    return (lon, lat, alt) in degrees, degrees, meters
+    '''
+    a = 6378137.0  # semi-major axis
+    e = 1 / 298.257223563  # flattening
+    e1s = 0.00669437999013  # 1st-eccentricity
+
+    L = np.arctan2(y, x)
+    tmpB = z / np.sqrt(x ** 2 + y ** 2)
+    B = np.arctan(tmpB / (1 - e) ** 2)
+
+    maxItrNum = 500
+
+    oldB = B
+    err = 10
+    thre = 1e-15
+    itrNum = 0
+
+    while err >= thre:
+        R1 = x / (np.cos(L) * np.cos(B))
+        R2 = a / np.sqrt(np.cos(B) * np.cos(B) + (1 - e1s)
+                         * np.sin(B) * np.sin(B))
+        B = np.arctan(tmpB * R1 / (R1 - R2 * e1s))
+        err = abs(oldB - B)
+        oldB = B
+        itrNum += 1
+        if itrNum > maxItrNum:
+            import warnings
+            warnings.warn(
+                f"max itreration number({maxItrNum}) meets in convertion from x-y-z to lla")
+            break
+    H = R1 - R2
+    lat = np.degrees(B)
+    lon = np.degrees(L)
+    alt = H
+    return lon, lat, alt
+
+
+def xyz2enu(xyz: np.ndarray, anchor) -> np.ndarray:
+    '''
+    xyz: (3, ) vector or (N, 3) array
+    anchor: [lon, lat, (0)] in degress
+    '''
+    lon, lat = anchor[0], anchor[1]
+    alt = anchor[2] if len(anchor) == 3 else 0
+    R = ll2Rne(lon, lat)
+    x0, y0, z0 = lla2xyz(lon, lat, alt)
+
+    return (xyz - np.r_[x0, y0, z0]) @ R
+
+
+def enu2xyz(enu: np.ndarray, anchor) -> np.ndarray:
+    '''
+    enu: (3, ) vector or (N, 3) array
+    anchor: [lon, lat, (0)] in degress
+    '''
+    lon, lat = anchor[0], anchor[1]
+    alt = anchor[2] if len(anchor) == 3 else 0
+    R = ll2Rne(lon, lat)
+    x0, y0, z0 = lla2xyz(lon, lat, alt)
+
+    return enu @ R.T + np.r_[x0, y0, z0]
+
+
+def enu2lla(enu: np.ndarray, anchor):
+    '''
+    xyz2lla \circ enu2xyz
+    enu should be (N, 3) 2d array
+    '''
+    xyz = enu2xyz(enu, anchor)
+    return xyz2lla(xyz[:, 0], xyz[:, 1], xyz[:, 2])
+
+
+def lla2enu(lla: np.ndarray, anchor):
+    '''
+    xyz2enu \circ lla2xyz
+    lla should be (N, 3) 2d array, in (degrees, degrees, meters)
+    '''
+    x, y, z = lla2xyz(lla[:, 0], lla[:, 1], lla[:, 2])
+    return xyz2enu(np.hstack((x, y, z)), anchor)
+
